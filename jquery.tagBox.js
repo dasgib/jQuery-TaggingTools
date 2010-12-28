cc7460806b7e1b0d4aa99215ecf273807cf90350
Daniel Stocks <daniel@webcloud.se>
2010-12-28 16:30:47 +0100

    first commit

---
+++ jquery.tagBox.js
@@ -0,0 +1,93 @@
+/*
+*   Dependencies:
+*   ------------
+*   jquery.autoGrowInput.js
+*/
+(function($) {
+    
+    function TagBox(input) {
+    
+        var self = this;
+        var tags = input.val().split(/\s*,\s*/);
+        self.input = input
+        self.tagInput = $('<input>', {
+            'type' : 'text',
+            'keydown' : function(e) {
+                if(e.keyCode == 13) {
+                    $(this).trigger("selectTag");
+                }
+            }
+        });
+        
+        self.tagInput.bind("selectTag", function() {
+            if(!$(this).val()) {
+                return;
+            }
+            self.addTag($(this).val());
+            $(this).val("");
+        });
+        
+        self.tagbox = $('<ul>', {
+            "class" : "tagbox",
+            click : function(e) {
+                self.tagInput.focus();
+            }
+        });
+
+        self.tags = []
+        
+        input.after(self.tagbox).hide();
+
+        self.inputHolder = $('<li class="input">');
+        self.tagbox.append(self.inputHolder);
+        self.inputHolder.append(self.tagInput);
+        self.tagInput.autoGrowInput();
+        
+        for(tag in tags) {    
+            self.addTag(tags[tag]);
+        }
+    }
+    
+    TagBox.prototype = {
+        
+        addTag : function(label) {
+            
+            var self = this;
+            var tag = $('<li class="tag">' + label + '</li>');
+            
+            this.tags.push(label);
+
+            tag.append($('<a>', {
+                "href" : "#",
+                "class": "close",
+                "text": "close",
+                click: function(e) {
+                    e.preventDefault();
+                    var index = self.tagbox.find("li").index($(this).parent());
+                    self.removeTag(index);
+                }
+            }));
+            self.inputHolder.before(tag);
+            self.updateInput();
+        },
+        removeTag : function(index) {
+            
+            this.tagbox.find("li").eq(index).remove();
+            this.tags.splice(index, 1);
+            this.updateInput();
+        },
+        updateInput : function() {
+            
+            this.input.val(this.tags.join(","));
+        }
+    }
+    
+    $.fn.tagBox = function() {
+        
+        return this.each(function() {
+            
+            var input = $(this);
+            var tagbox = new TagBox(input);
+        });
+    }
+})(jQuery);
